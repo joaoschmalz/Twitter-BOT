f651d41eb7e4ed3e33929b2a51a6e8e9dbdfd4bd
joao.schmalz <joaom.sch@outlook.com>
2023-02-28 11:27:32 -0300

    Ajustes

--- index.js
+++ index.js
@@ -1,101 +1,96 @@
 const rwClient = require("./twitterClient.js");
 const CronJob = require("cron").CronJob;
-const puppeteer = require ("puppeteer");
+const puppeteer = require("puppeteer");
 
-async function todayMatches(){
-    const browser = await puppeteer.launch({headless: true});
+async function todayMatches() {
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-    await page.goto('https://www.hltv.org/matches');
+    await page.goto("https://www.hltv.org/matches");
 
     const list = await page.evaluate(() => {
-        list = [];
-        upcomingMatches = document.querySelector('.upcomingMatchesSection');
-        matches = upcomingMatches.querySelectorAll('.upcomingMatch');
+        let list = [];
+        const upcomingMatches = document.querySelector(
+            ".upcomingMatchesSection"
+        );
+        const matches = upcomingMatches.querySelectorAll(".upcomingMatch");
 
-        for(let i = 0; i < matches.length; i++){
-            a = matches[i];
+        for (let match of matches) {
 
-            matchTime = a.querySelector('.matchTime').innerHTML;
-            matchMeta = a.querySelector('.matchMeta').innerHTML;
+            const matchTime = match.querySelector(".matchTime").innerHTML;
+            const matchMeta = match.querySelector(".matchMeta").innerHTML;
 
-            matchRating = a.querySelectorAll('.faded');
+            const matchRating = match.querySelectorAll(".faded");
 
-            teamOne = a.querySelector('.team1');
-            teamOneName = teamOne.querySelector('.matchTeamName').innerHTML;
+            const teamOne = match.querySelector(".team1");
+            const teamOneName = teamOne.querySelector(".matchTeamName").innerHTML;
 
-            teamTwo = a.querySelector('.team2');
-            teamTwoName = teamTwo.querySelector('.matchTeamName').innerHTML;
+            const teamTwo = match.querySelector(".team2");
+            const teamTwoName = teamTwo.querySelector(".matchTeamName").innerHTML;
 
-            matchEvent = a.querySelector('.matchEventName').innerHTML;
+            const matchEvent = match.querySelector(".matchEventName").innerHTML;
 
-            let match = {
+            let matchInfo = {
                 time: matchTime,
                 meta: matchMeta,
                 teamOne: teamOneName,
                 teamTwo: teamTwoName,
                 rating: matchRating.length,
-                event: matchEvent
-            }
+                event: matchEvent,
+            };
 
-            list.push(match)
+            list.push(matchInfo);
         }
         return list;
     });
     await browser.close();
     return list;
 }
-var tweets = [];
+let tweets = [];
 
-function populateListOfMatches(){
-    todayMatches().then(list => {
-        var tweetBody = '';
-        for (let i = 0; i < list.length; i++){
-            let tweetHeader = 'Today matches: \n';
-            let tweetFooter = '(+)';
-            matchInfo = list[i];
-            let string = matchInfo.teamOne + ' VS ' + matchInfo.teamTwo + ' at ' + matchInfo.time + ' in ' + matchInfo.meta + ' playing for ' + matchInfo.event + ' Game Rating ' + (5 - matchInfo.rating) + '/5;\n';
-            
-            if(i == 0){
+function populateListOfMatches() {
+    todayMatches().then((list) => {
+        let tweetBody = "";
+        for (let i = 0; i < list.length; i++) {
+            let tweetHeader = "Today matches: \n";
+            let tweetFooter = "(+)";
+            let matchInfo = list[i];
+            let rating = 5 - matchInfo.rating;
+            let string = `${matchInfo.teamOne} VS ${matchInfo.teamTwo} at ${matchInfo.time} in ${matchInfo.meta} playing for ${matchInfo.event} Game Rating ${rating}/5\n`;
+
+            if (i == 0) {
                 tweetBody = tweetBody + tweetHeader;
             }
-            if (tweetBody.length + string.length + tweetFooter.length< 280){
+            if (tweetBody.length + string.length + tweetFooter.length < 280) {
                 tweetBody = tweetBody + string;
             } else {
-                if(i !== list.length -1){
+                if (i !== list.length - 1) {
                     tweetBody = tweetBody + tweetFooter;
                 }
                 tweets.push(tweetBody);
                 tweetBody = string;
             }
-            if(i == list.length - 1){
+            if (i == list.length - 1) {
                 tweets.push(tweetBody);
-            }           
+            }
         }
 
-        for (let i = 0; i < tweets.length; i ++){
-            tweet(tweets[i]);
-        }        
+        for (let t of tweets) {
+            tweet(t);
+        }
     });
 }
 
-
-const tweet = async (finalTweet) => {
-    try{
+async function tweet (finalTweet) {
+    try {
         await rwClient.v1.tweet(finalTweet);
-        console.log('Posted!')
-    } catch(e){
+        console.log("Posted!");
+    } catch (e) {
         console.error(e);
     }
-}
+};
 
 const job = new CronJob("0 5 * * *", () => {
     populateListOfMatches();
 });
-
-// Teste post JOB
-// const job = new CronJob("* * * * *", () => {
-//     console.log('Starting job...')
-//     tweet();
-// });
 
 job.start();
